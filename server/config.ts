abfe9261ed29a1a7d60ff8cd3f06fc3a1e07042e
LiuWR <892684755@qq.com>
2022-06-22 23:35:05 +0800

    fix: 切换到远程mongodb

--- server/config.ts
+++ server/config.ts
@@ -2,4 +2,4 @@ export const PORT = 9910;
 
 export const SESSION_MAX_AGE = 1000 * 60; // cookie 过期时间
 
-export const DATABASE_URL = 'mongodb://127.0.0.1:27017/zblite';
+export const DATABASE_URL = 'mongodb://admin:oksht2020@jsframe.top:27017/zblite?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false';
