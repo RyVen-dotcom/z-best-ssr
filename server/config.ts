8c2062e2146bc825909ca5fd660dded10bb9cb25
LiuWRy <57699220+RyVen-dotcom@users.noreply.github.com>
2022-06-23 21:12:23 +0800

    Update config.ts
    hide url

--- server/config.ts
+++ server/config.ts
@@ -2,4 +2,4 @@ export const PORT = 9910;
 
 export const SESSION_MAX_AGE = 1000 * 60; // cookie 过期时间
 
-export const DATABASE_URL = 'mongodb://admin:oksht2020@jsframe.top:27017/zblite?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false';
+export const DATABASE_URL = 'mongodb://localhost:27017/zblite?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false';
